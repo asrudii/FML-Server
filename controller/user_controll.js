2319b349c989218291b51fc4c70e38419788e6de
asrudii <aseprudi.luqmanul@gmail.com>
2020-01-04 15:40:08 +0700

    ajsdasdf

--- controller/user_controll.js
+++ controller/user_controll.js
@@ -72,7 +72,7 @@ router.post('/login', function(req, res) {
             "failed":"error ocurred"
           })
         }else{
-          // console.log('The solution is: ', result),a
+          // console.log('The solution is: ', result),aasdf
           if(results.length >0){
             if(results[0].password == password){
                 const payload = {
