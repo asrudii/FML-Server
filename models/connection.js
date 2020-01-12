0112672bdeaf10172037a0e465852e93b13647a8
asrudii <aseprudi.luqmanul@gmail.com>
2020-01-12 22:09:58 +0700

    server db change

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : '35.239.176.240',
-  user     : 'fmldb',
-  password : 'asrudii08',
-  database : 'dbfml'
+  host     : 'remotemysql.com',
+  user     : 'kyVOxu6CyV',
+  password : 'XAfQwpgYc0',
+  database : 'kyVOxu6CyV'
 });
  
 connection.connect(function(error){
